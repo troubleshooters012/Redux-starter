925400d21d06713d8453d8ec6ca712d2bdb76dbe
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-08-11 11:02:46 -0700

    react-redux starter

--- src/Redux/Reducers/Reducers.js
+++ src/Redux/Reducers/Reducers.js
@@ -1,24 +1,15 @@
 const initialState = {
-    data: []
-}
-// fetch('url')
-// initialState.data = localStorage.getItem('pokeData') || [];
-
-const initialReducer = (state=initialState, action) => {
+    data: [{item: 'bought veggie'}]
+};
+const Reducer = (state = initialState, action) => {
     switch(action.type) {
         case 'ADD':
-            console.log(state);
-            state.data.push(action.item)
-            return state
-        break;
-        case 'Delete':
-            state.data.pop(action.item)
-        break;
+            state.data.push(action.payload)
+            return state;
+
         default:
-            state.data.push({item: 'I bought veggie'})
             return state;
     }
 }
 
-export default initialReducer;
-
+export default Reducer;
